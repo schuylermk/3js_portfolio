63afeff7937123bf72b67f5dda39a077228fb666
Schuyler Klaassen <schuyler.m.k@gmail.com>
2023-11-28 22:52:21 -0800

    Set up about section with 4 initial cards. Trouble with SectionWrapper due to parallax-tilt

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,6 +26,24 @@ const Hero = () => {
       </div>
 
       <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] border-4 border-secondary flex items-start justify-center rounded-3xl p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
