aa4a94905d881e1cde34401aa51feb14251cc09a
Schuyler Klaassen <schuyler.m.k@gmail.com>
2024-10-20 22:32:01 -0700

    Add firebase configuration

---
+++ firebase.js
@@ -0,0 +1,23 @@
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Import the functions you need from the SDKs you need
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA46GZ49CAj14PnSt8DWJ5bNg3ZMk8P_b8",
+  authDomain: "this-here-webligatory-weblog.firebaseapp.com",
+  projectId: "this-here-webligatory-weblog",
+  storageBucket: "this-here-webligatory-weblog.appspot.com",
+  messagingSenderId: "753683098144",
+  appId: "1:753683098144:web:2e95ef12eba96669ba6a45",
+  measurementId: "G-KJW5GQ6NKV",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db };
